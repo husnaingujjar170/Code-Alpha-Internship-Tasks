1995b13acc4891d0803d65c683cb65a0c54f3907
husnaingujjar170 <husnain.mahmood2002@gmail.com>
2024-07-16 15:22:47 +0500

    bawa

--- controllers/url.js
+++ controllers/url.js
@@ -1,9 +1,10 @@
-const { nanoid } = require("nanoid");
+const shortid = require("shortid");
+const URL = require("../models/ur;");
 async function handleGenerateNewShortURL(req,res)
 {
     const sbosy = req.body;
     if(!body.url) return res.status(400).json({error : 'url is required'})
-    const shortUrl = nanoid(8);
+    const shortUrl = shortid();
     await URL.createObjectURL({
         shortId: shortID,
         redirectURL: body.url,
